60f66daf2320da66f09a91842302400088d0c5e8
Howard Lo <howardlo@outlook.com>
2016-06-16 08:14:51 -0500

    init

---
+++ src/ExploreUnitTesting2Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ExploreUnitTesting2Test")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("b3b51257-e27d-4c78-9bbe-4c09f4606df5")]
---
+++ src/ExploreUnitTesting2Test/TestClass.cs
@@ -0,0 +1,36 @@
+﻿using ExploreUnitTesting2;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Owin.Testing;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExploreUnitTesting2Test
+{
+    [TestClass]
+    public class TestClass
+    {
+        [TestMethod]
+        public async Task TestStartup()
+        {
+            var host = new WebHostBuilder()
+			    .UseKestrel()
+			    .UseContentRoot(Directory.GetCurrentDirectory())
+			    .UseIISIntegration()
+			    .UseStartup<Startup>()
+			    .Build();
+
+            using (var server = TestServer.Create<Startup>())
+            {
+                var response = await server.HttpClient.GetAsync("/Home");
+                var body = await response.Content.ReadAsStringAsync();
+                Console.WriteLine("body: ");
+                Console.WriteLine(body);
+                Assert.IsTrue(body.Contains("Hello World!"));
+            }
+        }
+    }
+}
